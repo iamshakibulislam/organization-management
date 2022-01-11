55dd47a4ab0958dd2b2f2c1b6820ec383ad2fae3
unknown <iamshakibulislam@gmail.com>
2022-01-11 13:05:27 +0600

    changed staticfiles location

--- org_project/settings.py
+++ org_project/settings.py
@@ -122,7 +122,7 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfilesdata')
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfiles')]
 
 # Default primary key field type
