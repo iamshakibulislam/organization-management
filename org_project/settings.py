c8f182bb0606294ace506c71238f3f2de1420f55
unknown <iamshakibulislam@gmail.com>
2022-01-16 22:54:50 +0600

    ready to push

--- org_project/settings.py
+++ org_project/settings.py
@@ -123,7 +123,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfilesdirs')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfilesdirs')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
