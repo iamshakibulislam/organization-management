37df54a7f5459597eb5163ec3718bd28380234ec
unknown <iamshakibulislam@gmail.com>
2022-03-07 20:58:56 +0600

    edited profile name

--- org_project/settings.py
+++ org_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,7 +125,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-#STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfilesdirs')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfilesdirs')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -140,4 +140,4 @@ AUTH_USER_MODEL = 'users.User'
 #djanog_heroku settings 
 
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
